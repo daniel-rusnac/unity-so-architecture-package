e24f4355d79a3dd07d9d8c9be5856edd19f0c4be
Daniel <daniel.rusnac.personal@gmail.com>
2021-12-27 22:49:29 +0200

    Update Utility.cs

--- SO Architecture/Assets/SOArchitecture/Runtime/Utility.cs
+++ SO Architecture/Assets/SOArchitecture/Runtime/Utility.cs
@@ -2,6 +2,6 @@
 {
     internal static class Utility
     {
-        public const string CHANNEL_CREATION_MENU = "TMG/Channels/";
+        public const string CHANNEL_CREATION_MENU = "Channels/";
     }
 }
